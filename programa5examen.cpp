59f9dcb64debe29cb38efb0f67ab53a1616bc8d5
mastertigobisness <104740784+mastertigobisness@users.noreply.github.com>
2022-05-01 22:05:22 -0600

    Add files via upload

---
+++ programa5examen.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+
+{
+cout <<"numeros multiplos de 5, del 1 al 50"<<endl;
+for (int i=1; i<=50; i++)
+{
+if (i%5==0)(
+cout<<i<<endl);
+}
+system("PAUSE");
+return 0;
+}
+return 0;
+}
+
